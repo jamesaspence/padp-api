677858fb33ea0c46fead742524169444566807c6
James Spence <Jamesaspence20@gmail.com>
2018-01-27 13:15:06 -0600

    initial API for searching locations

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+require('dotenv').load();
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/locations/:pageToken?', (req, res) => {
+  //TODO retrieve five results at a time
+  const pageToken = req.params.pageToken;
+  if (pageToken) {
+    //TODO handle w/ page token, return next set of results
+  } else {
+    //TODO handle w/ lat long
+    const lat = req.query.lat;
+    const lng = req.query.lng;
+
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json`;
+    const config = {
+      params: {
+        location: `${lat},${lng}`,
+        rankby: `distance`,
+        type: `restaurant`,
+        key: process.env.GMAPS_API_KEY
+      }
+    };
+
+    axios.get(apiUrl, config)
+      .then(response => {
+        // console.log(response.data);
+        res.send(response.data);
+      }).catch(err => {
+        console.error(err);
+        //TODO return 500 error
+        res.status(500).send(JSON.stringify(err));
+      });
+  }
+});
+
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+  console.log(`Server listening at port ${port}.`);
+});
